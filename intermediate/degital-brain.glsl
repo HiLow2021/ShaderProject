df39eeea2895106600c6a97e73cd7b9de7aca3df
HiLow <newbie_wisdom@hotmail.com>
2023-12-03 12:48:15 +0900

    update

---
+++ intermediate/degital-brain.glsl
@@ -0,0 +1,129 @@
+// https://www.shadertoy.com/view/4sl3Dr
+// by srtuss, 2013
+precision mediump float;
+
+uniform float u_time;
+uniform vec2 u_resolution;
+
+// rotate position around axis
+vec2 rotate(vec2 p,float a)
+{
+    return vec2(p.x*cos(a)-p.y*sin(a),p.x*sin(a)+p.y*cos(a));
+}
+
+// 1D random numbers
+float rand(float n)
+{
+    return fract(sin(n)*43758.5453123);
+}
+
+// 2D random numbers
+vec2 rand2(in vec2 p)
+{
+    return fract(vec2(sin(p.x*591.32+p.y*154.077),cos(p.x*391.32+p.y*49.077)));
+}
+
+// 1D noise
+float noise1(float p)
+{
+    float fl=floor(p);
+    float fc=fract(p);
+    return mix(rand(fl),rand(fl+1.),fc);
+}
+
+// voronoi distance noise, based on iq's articles
+float voronoi(in vec2 x)
+{
+    vec2 p=floor(x);
+    vec2 f=fract(x);
+    
+    vec2 res=vec2(8.);
+    for(int j=-1;j<=1;j++)
+    {
+        for(int i=-1;i<=1;i++)
+        {
+            vec2 b=vec2(i,j);
+            vec2 r=vec2(b)-f+rand2(p+b);
+            
+            // chebyshev distance, one of many ways to do this
+            float d=max(abs(r.x),abs(r.y));
+            
+            if(d<res.x)
+            {
+                res.y=res.x;
+                res.x=d;
+            }
+            else if(d<res.y)
+            {
+                res.y=d;
+            }
+        }
+    }
+    return res.y-res.x;
+}
+
+void main(void)
+{
+    float flicker=noise1(u_time*2.)*.8+.4;
+    
+    vec2 uv=gl_FragCoord.xy/u_resolution.xy;
+    uv=(uv-.5)*2.;
+    vec2 suv=uv;
+    uv.x*=u_resolution.x/u_resolution.y;
+    
+    float v=0.;
+    
+    // that looks highly interesting:
+    //v = 1.0 - length(uv) * 1.3;
+    
+    // a bit of camera movement
+    uv*=.6+sin(u_time*.1)*.4;
+    uv=rotate(uv,sin(u_time*.3)*1.);
+    uv+=u_time*.4;
+    
+    // add some noise octaves
+    float a=.6,f=1.;
+    
+    for(int i=0;i<3;i++)// 4 octaves also look nice, its getting a bit slow though
+    {
+        float v1=voronoi(uv*f+5.);
+        float v2=0.;
+        
+        // make the moving electrons-effect for higher octaves
+        if(i>0)
+        {
+            // of course everything based on voronoi
+            v2=voronoi(uv*f*.5+50.+u_time);
+            
+            float va=0.,vb=0.;
+            va=1.-smoothstep(0.,.1,v1);
+            vb=1.-smoothstep(0.,.08,v2);
+            v+=a*pow(va*(.5+vb),2.);
+        }
+        
+        // make sharp edges
+        v1=1.-smoothstep(0.,.3,v1);
+        
+        // noise is used as intensity map
+        v2=a*(noise1(v1*5.5+.1));
+        
+        // octave 0's intensity changes a bit
+        if(i==0)
+        v+=v2*flicker;
+        else
+        v+=v2;
+        
+        f*=3.;
+        a*=.7;
+    }
+    
+    // slight vignetting
+    v*=exp(-.6*length(suv))*1.2;
+    
+    vec3 cexp=vec3(4.,2.,1.);
+    cexp*=1.4;
+    
+    vec3 col=vec3(pow(v,cexp.x),pow(v,cexp.y),pow(v,cexp.z))*2.;
+    
+    gl_FragColor=vec4(col,1.);
+}
